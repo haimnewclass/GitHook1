743a732f01440fdaf3e794decbab82897a8fe425
Haim Gilboa <56435506+haimnewclass@users.noreply.github.com>
2023-04-18 13:07:16 +0300

    Add project files.

---
+++ Sample.Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+namespace Sample.Test
+{
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
